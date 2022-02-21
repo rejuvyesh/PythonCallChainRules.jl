c6892be98829eef09fea3768ff8acbabbce993de
rejuvyesh <mail@rejuvyesh.com>
2022-02-21 23:49:37 +0000

    copy basic adaptors

--- src/PythonCallChainRules.jl
+++ src/PythonCallChainRules.jl
@@ -1,5 +1,24 @@
 module PythonCallChainRules
 
-# Write your package code here.
+using Requires
+
+import FillArrays
+import Adapt
+
+struct PyAdaptor{T} end
+Adapt.adapt_storage(to::PyAdaptor{T}, x::AbstractArray) where {T} = convert(Array, x)
+Adapt.adapt_storage(to::PyAdaptor{T}, x::StridedArray) where {T} = x
+Adapt.adapt_storage(to::PyAdaptor{T}, x::FillArrays.AbstractFill) where {T} = collect(x)
+
+
+include("pytorch.jl")
+include("jax.jl")
+
+function __init__()
+    @require CUDA = "052768ef-5323-5732-b1bb-66c8b64840ba" begin
+        include("cuda.jl")
+    end    
+end
+
 
 end

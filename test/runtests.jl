0122ed556c64e0f78904a9eaae5cc272f76fe81c
rejuvyesh <mail@rejuvyesh.com>
2022-02-22 18:25:35 +0000

    include tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
-using PythonCallChainRules
-using Test
+using SafeTestsets
 
-@testset "PythonCallChainRules.jl" begin
-    # Write your tests here.
+@safetestset "PythonCallChainRules.jl" begin
+    @safetestset "torch" begin
+        include("test_pytorch.jl")
+    end
+    @safetestset "jax" begin
+        include("test_jax.jl")
+    end
 end

46875738f1ab0d93c3b0fdb6004ab4ae3bf33069
rejuvyesh <mail@rejuvyesh.com>
2022-02-23 00:12:52 +0000

    still don't get where segfaults come from

--- src/pytorch.jl
+++ src/pytorch.jl
@@ -21,7 +21,7 @@ struct TorchModuleWrapper
     torch_stateless_module::Py
     dtype::Py
     params::Tuple
-    buffers::Tuple
+    buffers::Py
 end
 
 @functor TorchModuleWrapper (params,)
@@ -33,34 +33,32 @@ Base.iterate(f::TorchModuleWrapper) = iterate(f.params)
 Base.iterate(f::TorchModuleWrapper, state) = iterate(f.params, state)
 
 function TorchModuleWrapper(torch_module)
-    Bool(pyisinstance(torch_module, torch.nn.Module)) || error("Not a torch.nn.Module")
+    pyconvert(Bool, pyisinstance(torch_module, torch.nn.Module)) || error("Not a torch.nn.Module")
     funmod, params, buffers = functorch.make_functional_with_buffers(torch_module)
-    dtype = params[1].dtype
-    jlparams = map(params) do x
-        DLPack.wrap(x, dlpack.to_dlpack)
-    end
+    dtype = params[0].dtype
+    jlparams = pyconvert(Tuple, map(x->DLPack.wrap(x, dlpack.to_dlpack), params))
     return TorchModuleWrapper(funmod, dtype, jlparams, buffers)
 end
 
 function (wrap::TorchModuleWrapper)(args...; kwargs...)
     # TODO: handle multiple outputs
     params = wrap.params
-    tensor_out = wrap.torch_stateless_module(Tuple(map(x -> DLPack.share(x, Py, dlpack.from_dlpack).requires_grad_(true), params)),
+    tensor_out = wrap.torch_stateless_module((map(x -> DLPack.share(x, Py, dlpack.from_dlpack).requires_grad_(true), params)),
         wrap.buffers, map(x -> DLPack.share(x, Py, dlpack.from_dlpack), args)...; kwargs...)
-    res = DLPack.wrap(tensor_out, pyto_dlpack)
+    res = DLPack.wrap(tensor_out, dlpack.to_dlpack)
     return res
 end
 
 function ChainRulesCore.rrule(wrap::TorchModuleWrapper, args...; kwargs...)
     T = typeof(first(args))
     params = wrap.params
-    torch_primal, torch_vjpfun = functorch.vjp(@pyeval("buffer_implicit")(wrap.torch_stateless_module, wrap.buffers), Tuple(map(x -> DLPack.share(x, PyObject, pyfrom_dlpack).requires_grad_(true), params)),
+    torch_primal, torch_vjpfun = functorch.vjp(@pyeval("buffer_implicit")(wrap.torch_stateless_module, wrap.buffers), Tuple(map(x -> DLPack.share(x, Py, dlpack.from_dlpack).requires_grad_(true), params)),
         map(x -> DLPack.share(x, Py, dlpack.from_dlpack).requires_grad_(true), args)...; kwargs...)
     project = ProjectTo(args)
     function TorchModuleWrapper_pullback(Δ)
         torch_tangent_vals = torch_vjpfun(DLPack.share(Adapt.adapt(PyAdaptor{T}, Δ), Py, dlpack.from_dlpack))
-        jlparams_tangents = map(x -> (DLPack.wrap(x, pyto_dlpack)), torch_tangent_vals[1])
-        args_tangents = project(map(x -> (DLPack.wrap(x, pyto_dlpack)), torch_tangent_vals[2:end]))
+        jlparams_tangents = map(x -> (DLPack.wrap(x, dlpack.to_dlpack)), torch_tangent_vals[1])
+        args_tangents = project(map(x -> (DLPack.wrap(x, dlpack.to_dlpack)), torch_tangent_vals[2:end]))
         return (Tangent{TorchModuleWrapper}(; torch_stateless_module = NoTangent(), dtype = NoTangent(), params = jlparams_tangents, buffers = NoTangent()), args_tangents...)
     end
     res = DLPack.wrap(torch_primal, dlpack.to_dlpack)
@@ -87,7 +85,6 @@ function __init__()
     catch err
         @warn """PythonCallChainRules.jl has failed to import torch and functorch from Python.
                  Please make sure these are installed. 
-                 methods of this package.
         """
         @debug err
         ispysetup[] = false
--- test/test_pytorch.jl
+++ test/test_pytorch.jl
@@ -22,3 +22,69 @@ device = torch.device("cpu")
         @test isapprox(pyconvert(Float32, xto.cpu().sum().item()), sum(xjl))
     end
 end
+
+function compare_grad_wrt_params(modelwrap, inputs...)
+    params = map(x -> DLPack.share(x, Py, dlpack.from_dlpack).to(device = device, dtype = modelwrap.dtype).requires_grad_(true), (modelwrap.params))
+    torch_out = modelwrap.torch_stateless_module(params, modelwrap.buffers, map(z->DLPack.share(z, Py, dlpack.from_dlpack).to(dtype=modelwrap.dtype, device=device), inputs)...).sum()
+    torchgrad = map(x-> (x.cpu().numpy()), torch.autograd.grad(torch_out, params))
+    grad,  = Zygote.gradient(m->sum(m(inputs...)), modelwrap)
+    @test length(torchgrad) == length(grad.params)
+    for i in 1:length(grad.params)
+        @test isapprox(sum(torchgrad[i]), sum(grad.params[i]))
+    end
+    @test length(grad.params) == length(modelwrap.params)
+    @test grad.params[1] !== nothing
+    @test grad.params[2] !== nothing
+    @test size(grad.params[1]) == size(modelwrap.params[1])
+    @test size(grad.params[2]) == size(modelwrap.params[2])
+end
+
+function compare_grad_wrt_inputs(modelwrap, x)
+    params = map(z -> DLPack.share(z, Py, dlpack.from_dlpack).to(device = device, dtype = modelwrap.dtype).requires_grad_(true), (modelwrap.params))
+    xtorch = DLPack.share(copy(x), Py, dlpack.from_dlpack).to(dtype=modelwrap.dtype, device=device).requires_grad_(true)
+    torch_out = modelwrap.torch_stateless_module(params, modelwrap.buffers, xtorch).sum()
+    torchgrad = map(z-> (copy(z.cpu().numpy())), torch.autograd.grad(torch_out, xtorch))[1]
+    grad, = Zygote.gradient(z->sum(modelwrap(z)), x)
+    @test size(grad) == size(x)
+    @test length(torchgrad) == length(grad)
+    @test isapprox(sum(torchgrad), sum(grad))
+end
+
+batchsize = 1
+indim = 3
+outdim = 2
+hiddendim = 4
+
+#@testset "linear" begin
+    lin = torch.nn.Linear(indim, outdim).to(device=device)
+    linwrap = TorchModuleWrapper(lin)
+    # if CUDA.functional()
+    #     linwrap = fmap(CUDA.cu, linwrap)
+    # end    
+    x = randn(Float32, indim, batchsize)
+    # if CUDA.functional()
+    #     x = cu(x)
+    # end
+    y = linwrap(x)
+    @test size(y) == (outdim, batchsize)
+    compare_grad_wrt_params(linwrap, x)
+    compare_grad_wrt_inputs(linwrap, x)
+
+#end
+
+@testset "mlp" begin
+    mlp = torch.nn.Sequential(torch.nn.Linear(indim, hiddendim), torch.nn.ReLU(), torch.nn.Linear(hiddendim, outdim)).to(device=device)
+    mlpwrap = TorchModuleWrapper(mlp)
+    # if CUDA.functional()
+    #     mlpwrap = fmap(CUDA.cu, mlpwrap)
+    # end    
+
+    x = randn(Float32, indim, batchsize)
+    # if CUDA.functional()
+    #     x = cu(x)
+    # end
+    y = mlpwrap(x)
+    @test size(y) == (outdim, batchsize)
+    compare_grad_wrt_params(mlpwrap, x)
+    compare_grad_wrt_inputs(mlpwrap, x)
+end
